fa7c6a7ead4b2288e4750529bb02c0d9c9dfdb36
Álamo Vinícius <alamo.cma22@gmail.com>
2021-11-22 08:24:53 -0300

    Update ex 0115
    Desenvolvi a função deletar no meu mini sistema do exercício 115 do curso. (função não prevista pelo curso)

--- Curso Em Video/PYTHON (MUNDO 1, MUNDO 2 E MUNDO 3)/exercicios/ex0115_menu_operacoes/arquivo/__init__.py
+++ Curso Em Video/PYTHON (MUNDO 1, MUNDO 2 E MUNDO 3)/exercicios/ex0115_menu_operacoes/arquivo/__init__.py
@@ -1,4 +1,4 @@
-from interface import *
+import interface
 
 
 def arq_existe(nome):
@@ -20,7 +20,7 @@ def criar_arquivo(nome):
 def ler_arquivo(nome):
     try:
         with open(nome, 'rt') as a:
-            cabecalho('pessoas cadastradas')
+            interface.cabecalho('pessoas cadastradas')
             for linha in a:
                 dado = linha.split(';')
                 dado[1] = dado[1].replace('\n', '')
@@ -37,6 +37,22 @@ def cadastro_pessoa(arq, nome='desonhecido', age=0):
             except:
                 print('Houve um erro ao cadastrar nova pessoa')
             else:
-                print('usuário cadastrado com sucesso')
+                print(f'{nome} cadastrado com sucesso')
     except:
         print('erro ao cadastrar nova pessoa.')
+
+
+def deletar_pessoa(arq):
+    try:
+        nomes = open (arq, 'r').readlines()
+        for i, dado in enumerate(nomes):
+            print(f'{i}.............{dado[:-4]}')
+        choise = interface.leiaint('Escolha o número para deletar: ')
+        print(f'{nomes[choise][:-4]} Deletado com sucesso.')
+        del nomes[choise]
+        nomes = ''.join(nomes)
+        with open(arq, 'wt') as a:
+            a.write(nomes)
+    except IndexError:
+        print('\033[31merro, número inválido\033[m')
+        
--- Curso Em Video/PYTHON (MUNDO 1, MUNDO 2 E MUNDO 3)/exercicios/ex0115_menu_operacoes/main.py
+++ Curso Em Video/PYTHON (MUNDO 1, MUNDO 2 E MUNDO 3)/exercicios/ex0115_menu_operacoes/main.py
@@ -1,15 +1,15 @@
 """Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo de
 texto simples. O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as pessoas cadastradas."""
 
-from lib.arquivo import *
-from lib.interface import *
+from arquivo import *
+from interface import *
 from time import sleep
 
-arq = 'Curso em video.txt'
+arq = 'Data base.txt'
 if not arq_existe(arq):
     criar_arquivo(arq)
 while True:
-    resp = menu(['Exibir pessoas cadastradas', 'Cadastrar Pessoa', 'Sair'])
+    resp = menu(['Exibir pessoas cadastradas', 'Cadastrar Pessoa','Deletar uma pessoa', 'Sair'])
     if resp == 1:
         ler_arquivo(arq)
     elif resp == 2:
@@ -18,8 +18,11 @@ while True:
         idade = leiaint('Idade: ')
         cadastro_pessoa(arq, nome, idade)
     elif resp == 3:
+        cabecalho('DELETANDO')
+        deletar_pessoa(arq)
+    elif resp == 4:
         cabecalho('Saindo do sitema...')
         break
     else:
-        print('\033[91mErro, Digite uma opção inválida!\033[m' )
+        print('\033[91mErro, Digite uma opção nválida!\033[m' )
     sleep(1)
