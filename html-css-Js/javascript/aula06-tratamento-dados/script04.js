baa6e1726ad339ba0227bab3eeddbf0aaa432ac9
Álamo Vinícius <alamo.cma22@gmail.com>
2021-12-31 19:06:06 -0300

    Upload files

--- html-css-Js/javascript/aula06-tratamento-dados/script04.js
+++ html-css-Js/javascript/aula06-tratamento-dados/script04.js
@@ -1,6 +1,6 @@
 var nome = window.prompt('Escreva seu nome: ')
-document.write(`Olá <strong>${nome}</strong>, seu nome tem ${nome.length} letras<br>`)
-document.write(`Escrevendo seu nome em Upper case: ${nome.toUpperCase()}`)  //função para transformar em str maiuscula
+document.write(`<h1>Olá <strong>${nome}</strong>!</h1>, <p>seu nome tem ${nome.length} letras<br>`)
+document.write(`Escrevendo seu nome em Upper case: ${nome.toUpperCase()}</p>`)  //função para transformar em str maiuscula
 /* 
 n1.Tofixed(2) = converte para 2 casas reais em um dado float
 n2.tofixed(2).replace('.', ',') = alem de converter para 2 casas decimais, troca o ponto para virgula.
