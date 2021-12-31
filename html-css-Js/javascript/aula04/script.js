18b0a725adf86bb598b5a84d45b225cdab2d0b95
Álamo Vinícius <alamo.cma22@gmail.com>
2021-12-31 00:16:09 -0300

    files upload

---
+++ html-css-Js/javascript/aula04/script.js
@@ -0,0 +1,5 @@
+window.alert('Hello world!')
+window.confirm("Do you like to continue?")
+window.prompt("What's your name?")
+
+//repare que tanto faz usar aspas simples ou suplas, e não é necessário ultilizar (;) no final
